8d8d2a3d5839b90de2a9e1e88fd944a69dc39310
zhaqj12 <zhaqj12@naver.com>
2022-06-30 09:53:49 +0900

    Splash 화면(Intro), Handler postDelayed 사용

---
+++ app/src/main/java/com/kyeonghoon/lecture2twice/SplashActibity.kt
@@ -0,0 +1,18 @@
+package com.kyeonghoon.lecture2twice
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActibity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_actibity)
+        Handler().postDelayed({
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        },3000)
+    }
+
+}
