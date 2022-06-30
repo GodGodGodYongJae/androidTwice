e1c5b6b3e937afb958aedbbf44ac0166d11df363
zhaqj12 <zhaqj12@naver.com>
2022-06-30 10:09:30 +0900

    LinearLayout 화면 구성 scaleType=fitXY

--- app/src/main/java/com/kyeonghoon/lecture2twice/SplashActibity.kt
+++ app/src/main/java/com/kyeonghoon/lecture2twice/SplashActibity.kt
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 
+@Suppress("DEPRECATION")
 class SplashActibity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_actibity)
+
         Handler().postDelayed({
             startActivity(Intent(this,MainActivity::class.java))
             finish()
